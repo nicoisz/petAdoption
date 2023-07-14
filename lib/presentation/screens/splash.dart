d934f6024520d71bef533dc1d7f259dea660bd7c
nicoisz <nicolasignacio.sz@gmail.com>
2023-07-14 14:42:18 -0400

    improvement in code

--- lib/presentation/screens/splash.dart
+++ lib/presentation/screens/splash.dart
@@ -7,60 +7,98 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/slide-1.png',
-                height: 300,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Elije tu mascota',
-                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                  ),
-                  Icon(
-                    Icons.pets,
-                    size: 18.0,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text("Encuentra tu mejor futuro compañero")
-            ],
-          ),
-        ),
-        backgroundColor: const Color(0xFFb1e0da),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: GestureDetector(
-          onTap: () {},
-          child: Container(
-            width: 200,
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: Colors.white,
-            ),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 600,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                CircleButton(iconData: Icons.pets),
-                SizedBox(
-                  width: 10,
+                Transform.rotate(
+                  alignment: Alignment.center,
+                  origin: const Offset(-300.0, -100.0),
+                  angle: -75,
+                  child: const Icon(
+                    Icons.pets,
+                    size: 250.0,
+                  ),
                 ),
-                Text('GestureDetector')
+                Image.asset(
+                  'assets/images/slide-2.png',
+                  height: 200,
+                  width: 300,
+                  alignment: Alignment.bottomRight,
+                ),
               ],
             ),
           ),
-        ),
-      );
+          const SizedBox(
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Elije tu mascota',
+                      style:
+                          TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                    ),
+                    Icon(
+                      Icons.pets,
+                      size: 18.0,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Text("Encuentra tu mejor futuro compañero")
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: const Color(0xFFb1e0da),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.black),
+              ),
+            ),
+            child: const Text('hechar un vistazo'),
+          ),
+          const SizedBox(
+            width: 60,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: 160,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.white,
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleButton(iconData: Icons.pets),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text('Registrarse')
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
