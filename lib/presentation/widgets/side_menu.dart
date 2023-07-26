5492cd59e9d23ecbe5f29a374c218211c4de1963
nicoisz <nicolasignacio.sz@gmail.com>
2023-07-26 13:28:07 -0400

    creratin drawer

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -9,8 +9,8 @@ class SideMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: const <Widget>[
-          SizedBox(
+        children: <Widget>[
+          const SizedBox(
             height: 80,
             child: DrawerHeader(
               child: Text(
@@ -23,10 +23,58 @@ class SideMenu extends StatelessWidget {
             ),
           ),
           ListTile(
-            title: Text('Item 1'),
+            onLongPress: () {
+              print("press");
+            },
+            leading: const Icon(Icons.person),
+            title: const Text('Profile'),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 14,
+            ),
           ),
           ListTile(
-            title: Text('Item 2'),
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 14,
+            ),
+            onLongPress: () {
+              print("press");
+            },
+          ),
+          //line
+          ListTile(
+            leading: const Icon(Icons.info_outline_rounded),
+            title: const Text('Information'),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 14,
+            ),
+            onLongPress: () {
+              print("press");
+            },
+          ),
+          const ListTile(
+            leading: Icon(Icons.dark_mode_outlined),
+            title: Text('Dark mode'),
+            trailing: Switch(
+              activeTrackColor: Colors.green,
+              value: true,
+              onChanged: null,
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Log out'),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 14,
+            ),
+            onLongPress: () {
+              print("press");
+            },
           ),
         ],
       ),
