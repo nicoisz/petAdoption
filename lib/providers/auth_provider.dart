5edf451e8afe6b281c303362133c72aa78205889
nicoisz <nicolasignacio.sz@gmail.com>
2023-08-28 08:02:57 -0400

    implementing riverpod with firebase auth

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:petadoption/repository/auth_repository.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepository(FirebaseAuth.instance);
+});
---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository {
+  const AuthRepository(this._auth);
+
+  final FirebaseAuth _auth;
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    final result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return result.user;
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
+
+class AuthException implements Exception {
+  final String message;
+
+  AuthException(this.message);
+
+  @override
+  String toString() {
+    return message;
+  }
+}
